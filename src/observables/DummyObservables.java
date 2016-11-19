de1566c729170ca181b9262267bbd461e388d928
Nariun <jonathan_brites@hotmail.com>
2016-11-19 18:38:53 +0100

    Added justObservable + fromObservable + rangeObservable

--- src/observables/DummyObservables.java
+++ src/observables/DummyObservables.java
@@ -6,6 +6,7 @@ import rx.Subscriber;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.Arrays;
 
 /**
  * Created by Jonathan Brites on 19/11/2016.
@@ -15,39 +16,52 @@ public class DummyObservables {
         final Subscriber<String> subscriber = new Subscriber<String>() {
             @Override
             public void onCompleted() {
-                log("I'm done");
+                log("Observable said he's done");
             }
 
             @Override
             public void onError(Throwable throwable) {
-                log("Something went wrong!");
+                log("Something went wrong with the observable!");
                 log(createStringFromStacktrace(throwable));
             }
 
             @Override
             public void onNext(String s) {
-                log("Something came in: " + s);
+                log("Observable told me: " + s);
             }
         };
 
-        subscribeToErrorObservable(subscriber);
+//        subscribeToErrorObservable(subscriber);
 //        subscribeToNeverObservable(subscriber);
 //        subscribeToEmpty(subscriber);
+//        subscribeToJustObservable(subscriber);
+//        subscribeToFromObservable(subscriber);
+        subscribeToRangeObservableWithLambas();
 
     }
 
+    private static void subscribeToFromObservable(final Subscriber<String> subscriber) {
+        final Observable<String> fromObservable = Observable.from(Arrays.asList("First message", "Second message", "Third message", "Fourth message"));
+        subscribeToObservable(fromObservable, subscriber);
+    }
+
+    private static void subscribeToJustObservable(final Subscriber<String> subscriber) {
+        final Observable<String> justObservable = Observable.just("I only sent one value");
+        subscribeToObservable(justObservable, subscriber);
+    }
+
     private static void subscribeToEmpty(final Subscriber<String> subscriber) {
-        Observable<String> emptyObservable = Observable.empty();
+        final Observable<String> emptyObservable = Observable.empty();
         subscribeToObservable(emptyObservable, subscriber);
     }
 
     private static void subscribeToNeverObservable(final Subscriber<String> subscriber) {
-        Observable<String> neverObservable = Observable.never();
+        final Observable<String> neverObservable = Observable.never();
         subscribeToObservable(neverObservable, subscriber);
     }
 
     private static void subscribeToErrorObservable(final Subscriber<String> subscriber){
-        Observable<String> errorObservable = Observable.error(new Exception("This error will destroy the world!"));
+        final Observable<String> errorObservable = Observable.error(new Exception("This error will destroy the world!"));
         subscribeToObservable(errorObservable, subscriber);
     }
 
@@ -62,9 +76,18 @@ public class DummyObservables {
     }
 
     private static String createStringFromStacktrace(final Throwable throwable){
-        StringWriter sw = new StringWriter();
-        PrintWriter pw = new PrintWriter(sw);
+        final StringWriter sw = new StringWriter();
+        final PrintWriter pw = new PrintWriter(sw);
         throwable.printStackTrace(pw);
         return sw.toString();
+    }
+
+    private static void subscribeToRangeObservableWithLambas() {
+        final Observable<Integer> rangeObservable = Observable.range(5,20);
+        log("Before subscription");
+        rangeObservable.subscribe(value -> log(value.toString()),
+                throwable -> log(createStringFromStacktrace(throwable)),
+                () -> log("Observable said he's done"));
+        log("After subscription");
     }
 }
